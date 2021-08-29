ba64f6c784fdc54c261cef5a4f897eda3eb12eea
Kobato13 <johncarlomartinez13@gmail.com>
2021-08-29 19:25:27 +0800

    momentum app update

--- momentum-app/app.js
+++ momentum-app/app.js
@@ -28,6 +28,9 @@ function greetings(){
   setGreetings.classList.add("fade-in")
   setFocus.style.display = "flex"
   setTime.style.display = "block"
+  todoToggle.style.visibility = "visible"
+  backgroundToggle.style.visibility = "visible"
+  quotesMain.style.visibility = "visible"
 }
 function focusFunction(){
   focusForm.style.display = "none"
@@ -49,7 +52,7 @@ setInterval(function(){
   hours = hours % 12;
   hours = hours ? hours : 12;
 
-  setTime.innerHTML = "<h1>" + hours + ":" + minutes + ":" + seconds + " " + ampm + "</h1>"
+  setTime.innerHTML = "<h1>" + hours + ":" + minutes + " " + ampm + "</h1>"
 }, 1000)
 
 function addZero(num) {
@@ -146,6 +149,7 @@ function ToggleBackground() {
 }
 
 
+const quotesMain = document.querySelector(".quotes-container")
 const quoteContainer = document.querySelector(".quotes-container2")
 const quoteText = document.createElement("p")
 quoteContainer.appendChild(quoteText)
@@ -153,13 +157,35 @@ const newQuote = document.querySelector(".new-quote")
 
 
 const getQuote = async() => {
+  quoteContainer.style.display = 'block'
   const res = await fetch("https://type.fit/api/quotes")
   const quotes = await res.json()
   const num = Math.floor(Math.random()*quotes.length)
   const item = quotes[num]
   const quote = item.text
+  quoteForm.style.visibility = 'hidden'
   quoteText.innerHTML = quote
 }
 
 newQuote.addEventListener('click', getQuote)
 getQuote()
+
+const addQuote = document.querySelector(".add-quote")
+const quoteForm = document.querySelector("#quote-form")
+const quoteInput = document.querySelector("#quote-input")
+const quoteText2 = document.createElement("p")
+quoteForm.appendChild(quoteText2)
+
+addQuote.addEventListener('click', addedQuote)
+
+function addedQuote() {
+  quoteForm.style.visibility = 'visible'
+  quoteContainer.style.display = 'none'
+}
+
+quoteForm.addEventListener('submit', function(e){
+  e.preventDefault()
+  quoteText.innerHTML = quoteInput.value
+  quoteForm.style.visibility = 'hidden'
+  quoteContainer.style.display = 'block'
+})
