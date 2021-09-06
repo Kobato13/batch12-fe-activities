6dc63c185dacff5e74b3fd930aa01bf3411da747
Kobato13 <johncarlomartinez13@gmail.com>
2021-09-06 20:38:12 +0800

    Initial javascript code and clicking through a cell will display X

---
+++ tictactoe/js/index.js
@@ -0,0 +1,36 @@
+const cells = Array.from(document.querySelectorAll('.cell'))
+const playerDisplay = document.querySelector('.display-player')
+const winningPlayer = document.querySelector('.winner')
+const resetButton = document.querySelector('.reset-button')
+
+let boardState = ['', '', '', // board state in 3 by 3
+                  '', '', '', // we will fill the array
+                  '', '', '']
+let currentPlayer = 'X'
+
+
+// Looping through the array of cells that the cell has been clicked
+cells.forEach(function(cell, index) {
+  cell.addEventListener('click', function(){
+    cell.innerHTML = currentPlayer
+    boardState[index] = currentPlayer
+    resultValid()
+    playerChange()
+  })
+})
+
+// Validation if the game is over or won by whoever
+function resultValid() {
+
+}
+
+// Player changing between X and O
+function playerChange() {
+
+}
+
+// Restarting the game and the array of the board
+function restartGame() {
+
+}
+resetButton.addEventListener('click', restartGame)
